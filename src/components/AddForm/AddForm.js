04edb1c8e4dc4d546e12c705926a95b00f2fad72
Yashar <yashar22mo@gmail.com>
2024-07-10 21:02:39 +0330

    Add click handler function fot button

--- src/components/AddForm/AddForm.js
+++ src/components/AddForm/AddForm.js
@@ -4,6 +4,16 @@ import './AddForm.css'
 export default class AddForm extends React.Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+
+        }
+
+        this.clickHandler = this.clickHandler.bind(this)
+    }
+
+    clickHandler() {
+        console.log('sdf');
     }
 
     render() {
@@ -11,16 +21,16 @@ export default class AddForm extends React.Component {
             <div className="add-form">
 
                 <div className="title">
-                <label className="title_label label" for="title">Title :</label>
+                <label className="title_label label" htmlFor="title">Title :</label>
                 <input className="title_input input" id="title" maxLength={30}></input>
                 </div>
 
                 <div className="author">
-                <label className="author_label label" for="author">Author :</label>
+                <label className="author_label label" htmlFor="author">Author :</label>
                 <input className="author_input input" id="author" maxLength={30}></input>
                 </div>
 
-                <button className="button">Add Book</button>
+                <button className="button" onClick={this.clickHandler}>Add Book</button>
 
             </div>
         )
